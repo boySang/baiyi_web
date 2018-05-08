a5993d86572036b9b0f2d6abfb65b0203e6a23f2
boySang <sxsjl2015@126.com>
2018-05-08 23:52:50 +0800

    完善专家栏目

--- web/Home/Controller/ExpertsWendaController.class.php
+++ web/Home/Controller/ExpertsWendaController.class.php
@@ -27,4 +27,27 @@ class ExpertsWendaController extends LayoutController {
 		$m = D('ExpertsWenda');
 		echo $m->ajaxGetAll();
 	}
+
+	public function ajaxGetPageNews(){
+		header('Content-type:text/json');
+		$m = D('ExpertsWenda');
+		echo $m->ajaxGetPageNews();
+	}
+
+	public function detail($id){
+		if(!is_numeric($id)){
+			$this->error('参数错误');
+		}
+		$m = D('ExpertsWenda');
+		if(F('ExpertsWendaDetailId_'.$id)){
+			$data = F('ExpertsWendaDetailId_'.$id);
+		}else{
+			$data = $m->getOne($id);
+			F('ExpertsWendaDetailId_'.$id,$data);
+		}
+		$this->assign(array(
+			'data'			=>			$data,
+		));
+		$this->display();
+	}
 }
--- web/Home/Model/ExpertsWendaModel.class.php
+++ web/Home/Model/ExpertsWendaModel.class.php
@@ -9,6 +9,9 @@ class ExpertsWendaModel extends Model{
 		$d = $this->field('id,title,answer,addtime')->where('pid=%d',$pid)->limit($limit)->select();
 		foreach($d as $k=>$v){
 			$d[$k]['addtime'] = date('Y-m-d',$v['addtime']);
+			if($pid == 9){
+				$d[$k]['url'] = U('ExpertsWenda/detail/id/'.$v['id']);
+			}
 		}
 		return $d;
 	}
@@ -18,22 +21,21 @@ class ExpertsWendaModel extends Model{
 		if(!$topNav){
 			return returnApi(201,'获取失败');
 		}
-		$_temparr = array();
-		foreach($topNav as $k=>$v){
+		if(F('ExpertsWendaAllArticle')){
+			$_temparr = F('ExpertsWendaAllArticle');
+		}else{
+			$_temparr = array();
+			foreach($topNav as $k=>$v){
 
-			if($v['id'] == (1)){
-				unset($topNav[$k]);
-				continue;
-			}
-
-			if($v['id'] == 9){
-
-				$where = 'pid='.$v['id'];
+				if($v['id'] == (1)){
+					unset($topNav[$k]);
+					continue;
+				}
 
 		        //每页条数
 		        $pagesize = 8;
 
-		        $total = $this->where($where)->count();
+		        $total = $this->where('pid=%d',$v['id'])->count();
 
 				$page = new \Think\AjaxPage($total,$pagesize);
 
@@ -45,53 +47,111 @@ class ExpertsWendaModel extends Model{
 		        //生成翻页字符串
 		        $show = $page->show();
 
-		        $data = $this->field('id,title,pid,answer,addtime')->where($where)->order('id DESC')->limit($page->firstRow,$page->listRows)->select();
+		        $data = $this->field('id,title,pid,answer,addtime')->where('pid=%d',$v['id'])->order('id DESC')->limit($page->firstRow,$page->listRows)->select();
 
 		        if($data){
-		        	foreach($data as $k1=>$v1){
-		        		$data[$k1]['url'] = U('ExpertsWenda/detail/id/'.$v['id']);
-		        		$data[$k1]['addtime'] = date('Y-m-d',$v1['addtime']);
-		        	}
+					if($v['id'] == 9){
+			        	foreach($data as $k1=>$v1){
+			        		$data[$k1]['url'] = U('ExpertsWenda/detail/id/'.$v1['id']);
+			        		$data[$k1]['addtime'] = date('Y-m-d',$v1['addtime']);
+			        	}
+			        }
 		        	$topNav[$k]['main'] = array(
 			        	'data'		=>		$data,
 			        	'page'		=>		$show,
 			        );
 		        }
 
-			}else{
 
-				$where = 'pid='.$v['id'];
-
-		        //每页条数
-		        $pagesize = 15;
-
-		        $total = $this->where($where)->count();
-
-				$page = new \Think\AjaxPage($total,$pagesize);
-
-		        $page->setConfig('prev','上一页');
-		        $page->setConfig('next','下一页');
-		        $page->setConfig('first','首页');
-		        $page->setConfig('last','末页');
-
-		        //生成翻页字符串
-		        $show = $page->show();
-
-		        $data = $this->field('id,title,pid,answer')->where($where)->order('id DESC')->limit($page->firstRow,$page->listRows)->select();
-
-		        if($data){
-		        	$topNav[$k]['main'] = array(
-			        	'data'		=>		$data,
-			        	'page'		=>		$show,
-			        );
-		        }
-		    }
-
-		}
-		foreach($topNav as $k=>$v){
-			$_temparr[] = $v;
+			}
+			foreach($topNav as $k=>$v){
+				$_temparr[] = $v;
+			}
+			F('ExpertsWendaAllArticle',$_temparr);
 		}
 		return returnApi(200,'success',$_temparr);
+	}
+
+	public function ajaxGetPageNews(){
+		$articletype = I('get.articletype');
+		if(!is_numeric($articletype)){
+			return returnApi(201,'参数错误');
+		}
+
+		if(F('ExpertsWendaAllArticleFromid_'.$articletype.'_p_'.I('get.p'))){
+			$_data = F('ExpertsWendaAllArticleFromid_'.$articletype.'_p_'.I('get.p'));
+			if($_data){
+				if($articletype == 9){
+	        		$state = 20000;
+				}else{
+	        		$state = 30000;
+				}
+				$msg = 'success';
+	        }else{
+				if($articletype == 9){
+	        		$state = 20001;
+				}else{
+	        		$state = 30001;
+				}
+	        	$msg = '未查询到数据';
+	        }
+		}else{
+
+	        //每页条数
+	        $pagesize = 8;
+
+	        $total = $this->where('pid=%d',$articletype)->count();
+
+			$page = new \Think\AjaxPage($total,$pagesize);
+
+	        $page->setConfig('prev','上一页');
+	        $page->setConfig('next','下一页');
+	        $page->setConfig('first','首页');
+	        $page->setConfig('last','末页');
+
+	        //生成翻页字符串
+	        $show = $page->show();
+
+	        $data = $this->field('id,title,pid,answer,addtime')->where('pid=%d',$articletype)->order('id DESC')->limit($page->firstRow,$page->listRows)->select();
+
+	        if($data){
+				if($articletype == 9){
+		        	foreach($data as $k=>$v){
+		        		$data[$k]['url'] = U('ExpertsWenda/detail/id/'.$v['id']);
+		        		$data[$k]['addtime'] = date('Y-m-d',$v['addtime']);
+		        	}
+	        		$state = 20000;
+				}else{
+	        		$state = 30000;
+				}
+				$msg = 'success';
+	        	$_data['data'] = $data;
+	        	$_data['page'] = $show;
+	        }else{
+				if($articletype == 9){
+	        		$state = 20001;
+				}else{
+	        		$state = 30001;
+				}
+	        	$msg = '未查询到数据';
+	        }
+	        F('ExpertsWendaAllArticleFromid_'.$articletype.'_p_'.I('get.p'),$_data);
+			$_data = F('ExpertsWendaAllArticleFromid_'.$articletype.'_p_'.I('get.p'));
+	    }
+        return returnApi($state,$msg,$_data);
+	}
+
+	public function getOne($id){
+		$d = $this->field('id,title,pid,answer,addtime')->find($id);
+		$d['pidname'] = $this->getTitle($d['pid']);
+		$d['addtime'] = date('Y-m-d',$d['addtime']);
+		$d['answer'] = htmlspecialchars_decode($d['answer']);
+		return $d;
+	}
+
+	public function getTitle($pid){
+		$d = $this->field('title')->where('id=%d',$pid)->find();
+		return $d['title'];
 	}
 
 	public function getTopNav(){
